1f8793441df819452a5b4a41ea5488ad04d71af7
DESKTOP-FK0MJUV\JS <kjsj20@naver.com>
2020-10-19 15:07:49 +0900

    오전수업커밋!

---
+++ project1019/DataType2.java
@@ -0,0 +1,11 @@
+class DataType2
+{
+	public static void main(String[] args){
+		char a = '한';
+//		char b = true;//char는 문자와 숫자형을 넣을 수 있다. 이때의 숫자는 키코드 값임
+		char c = 65; //char 는 사실상, 숫자 자료형임!!
+
+		byte b = -128;
+		char x = -129;
+	}
+}
